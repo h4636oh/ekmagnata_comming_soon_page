f91038d498099bdbed16c63cdbbde7f2aecf5ea6
h4636oh <h4636oh@tuta.io>
2025-09-17 22:16:46 +0530

    font error fixed, coming soon added

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,38 @@
-const Title = ({content}) => {
+import { useEffect, useState } from "react";
+
+const Title = () => {
+  const texts = ["Ekmāgnatā", "!! Coming Soon !!"];
+  const [text, setText] = useState("");
+  const [index, setIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const current = texts[index % texts.length];
+    let typingSpeed = isDeleting ? 50 : 100;
+
+    const handleTyping = () => {
+      if (!isDeleting && text === current) {
+        setTimeout(() => setIsDeleting(true), 1000);
+      } else if (isDeleting && text === "") {
+        setIsDeleting(false);
+        setIndex((prev) => prev + 1);
+      } else {
+        setText(
+          isDeleting
+            ? current.substring(0, text.length - 1)
+            : current.substring(0, text.length + 1)
+        );
+      }
+    };
+
+    const timer = setTimeout(handleTyping, typingSpeed);
+    return () => clearTimeout(timer);
+  }, [text, isDeleting, index, texts]);
+
   return (
-    <h1 className='text-6xl lg:text-8xl text-[#FBF6EA] font-bold text-center underline underline-offset-7 px-4'>
-      {content}
+    <h1 className="text-6xl lg:text-8xl text-[#FBF6EA] font-bold text-center px-4">
+      {text}
+      <span className="animate-pulse">|</span>
     </h1>
   );
 };
